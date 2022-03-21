88931cdd70446e228737d4befc2339dadbd943f7
carolinadelcarpio <carolina.carpio.peralta@gmail.com>
2022-03-20 22:02:22 -0500

    version3

---
+++ esferalabcarolina/src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+const   NavBar = () =>{
+    return(
+        <nav>
+            <button type="button" class="btn btn-primary">Home</button>
+            <button type="button" class="btn btn-primary">Servicios</button>
+            <button type="button" class="btn btn-primary">Cuenta</button>
+            <button type="button" class="btn btn-primary">Carrito</button>
+        </nav>
+    )
+}
+
+export default NavBar;
