099eeaf81133a4169910de3a5fc363e68636fff1
carolinadelcarpio <carolina.carpio.peralta@gmail.com>
2022-04-09 23:54:50 -0500

    version20

--- esferalabcarolina/src/Components/ItemListContainer/ItemDetail.js
+++ esferalabcarolina/src/Components/ItemListContainer/ItemDetail.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
-const ItemDetail =()=> {
-        const[products,setProducts]=useState ([
-                {id: 1, Nombre: 'AA', Unidades: 1, Precio: 5, Imagen: <img src='images/imag1.PNG' alt='1'></img>},
-                {id: 2, Nombre: 'AB', Unidades: 5, Precio: 15, Imagen: <img src='images/imag2.PNG'alt='2'></img>},
-                {id: 3, Nombre: 'AC', Unidades: 12, Precio: 25, Imagen: <img src='images/imag3.PNG' alt='3'></img>},
-        ])
-        return <ul>
-        {products.map(u => <li key={u.id}>{u.Nombre}</li>)}
-        {products.map(u => <li key={u.id}>{u.Unidades}</li>)}
-        {products.map(u => <li key={u.id}>{u.Precio}</li>)}
-        {products.map(u => <li key={u.id}><img alt='0'>{u.Imagen}</img></li>)}
-        </ul>
+import FunctionCounter from '../FunctionCounter/FunctionCounter';
+import Item from './Item';
+//const ItemDetail =()=> {
+        //const[products,setProducts]=useState ([
+        //      {id: 1, Nombre: 'AA', Unidades: 1, Precio: 5, Imagen: <img src='images/imag1.PNG' alt='1'></img>},
+        //    {id: 2, Nombre: 'AB', Unidades: 5, Precio: 15, Imagen: <img src='images/imag2.PNG'alt='2'></img>},
+        //        {id: 3, Nombre: 'AC', Unidades: 12, Precio: 25, Imagen: <img src='images/imag3.PNG' alt='3'></img>},
+        //])
+        //return <ul>
+        //<FunctionCounter/>
+        //{products.map(u => <li key={u.id}>{u.Nombre}</li>)}
+        //{products.map(u => <li key={u.id}>{u.Unidades}</li>)}
+        //{products.map(u => <li key={u.id}>{u.Precio}</li>)}
+        //{products.map(u => <li key={u.id}><img alt='0'>{u.Imagen}</img></li>)}
+        //</ul>
+//}
+function ItemDetail({ Item }) {
+        onAdd(quantityToAdd); {
+        // Hemos recibido un evento del ItemCount
+        }
+return <>
+            <FunctionCounter/> // Configura las props de ItemCount. Qué habría que mandarle?👀
+</>;
 }
+
 
 export default ItemDetail;
